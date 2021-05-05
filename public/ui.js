624700f7710ae6f13991cd27c42e7f9fafcc9b48
Ryan <rbuxton1@gmail.com>
2021-05-05 11:55:37 -0600

    ok yeah good now ish

--- public/ui.js
+++ public/ui.js
@@ -3,38 +3,39 @@ $(document).ready(() => {
   $("#go").click(() => {
 
     $.get(`/${$("#pokemon").val()}`, data => {
+      $("#doubleTo").text("");
+      $("#halfTo").text("");
+      $("#noneTo").text("");
+      $("#doubleFrom").text("");
+      $("#halfFrom").text("");
+      $("#noneFrom").text("");
+
       data.forEach((type, i) => {
         console.log(JSON.stringify(type.damage_relations));
 
         type.damage_relations.double_damage_to.forEach((item, i) => {
-          var tr = `<tr>${item.name}</tr>`;
-          console.log(tr)
+          var tr = item.name + "<br>";
           $("#doubleTo").append(tr);
         });
         type.damage_relations.half_damage_to.forEach((item, i) => {
-          var tr = `<tr>${item.name}</tr>`;
-          console.log(tr)
+          var tr = item.name + "<br>";
           $("#doubleTo").append(tr);
         });
         type.damage_relations.no_damage_to.forEach((item, i) => {
-          var tr = `<tr>${item.name}</tr>`;
-          console.log(tr)
+          var tr = item.name + "<br>";
           $("#doubleTo").append(tr);
         });
 
         type.damage_relations.double_damage_from.forEach((item, i) => {
-          var tr = `<tr>${item.name}</tr>`;
-          console.log(tr)
+          var tr = item.name + "<br>";
           $("#doubleTo").append(tr);
         });
         type.damage_relations.half_damage_from.forEach((item, i) => {
-          var tr = `<tr>${item.name}</tr>`;
-          console.log(tr)
+          var tr = item.name + "<br>";
           $("#doubleTo").append(tr);
         });
         type.damage_relations.no_damage_from.forEach((item, i) => {
-          var tr = `<tr>${item.name}</tr>`;
-          console.log(tr)
+          var tr = item.name + "<br>";
           $("#doubleTo").append(tr);
         });
       });
